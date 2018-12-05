efdc2f4909cdbfc920f5030dcea5ce42e4892f5e
ben <groever@g.harvard.edu>
2018-12-04 23:50:35 -0500

    get val is working

--- autodiff/interface/interface.py
+++ autodiff/interface/interface.py
@@ -13,80 +13,7 @@ class AutoDiff:
         self.ndim = ndim
         sig = inspect.signature(self.fn)
         self.l = len(list(sig.parameters))
-    #     self.dual = {}
-    #     # self.dual.der = None
-    #     # self.dual.val = None
-    #     # self.dual = {'val':None, 'der':None}
-    #
-    # def get_der(self, input_val):
-    #     # if self.dual.der is not No ne:
-    #     if 'der' in self.dual:
-    #         return self.dual['der']
-    #     self.preprocess(input_val)
-    #     return self.dual['der']
-    #
-    # def get_val(self, input_val):
-    #     if self.dual.val is not None:
-    #         return self.dual['val']
-    #     self.preprocess(input_val)
-    #     return self.dual['val']
-    #
-    # def preprocess(self, input_val):
-    #     ders = []
-    #
-    #     if self.ndim >1:
-    #         vals = []
-    #         for i in range(self.ndim):
-    #             def fxn(*args):
-    #                 return self.fn(*args)[i]
-    #             a = AutoDiff(fxn,ndim=1)
-    #             a.l=self.l
-    #             ders.append(a.get_der(input_val))
-    #             vals.append(a.get_val(input_val))
-    #         self.dual['der'] = ders
-    #         self.dual['val'] = vals
-    #     else:
-    #         vals = None #[]
-    #         if self.l >= 2:
-    #             #for list of lists, each list evaluated at different variables
-    #             if any(isinstance(el, list) for el in input_val):
-    #                 list_der = []
-    #                 list_vals = []
-    #                 for p in input_val:
-    #                     list_der.append(self.get_der(p))
-    #                     list_vals.append(self.get_val(p))
-    #                     self.dual['der'] = list_ders
-    #                     self.dual['val'] = list_vals
-    #                 # return list_der
-    #             elif self.l != len(input_val):
-    #                 raise Exception('Function requires {} values that correspond to the multiple variables'.format(self.l))
-    #             else:
-    #                 #for a list of numbers, evaluated at different variables.
-    #                 for i in range(self.l):
-    #                     new_val = input_val.copy()
-    #                     new_val[i] = Dual(new_val[i])
-    #                     v = self.fn(*new_val)
-    #                     #Check if variable is in the function. (E.g., function paramaters are x, y and function is x.)
-    #                     if type(v) is Dual:
-    #                         ders.append(self.fn(*new_val).der)
-    #                     else:
-    #                         ders.append(0)
-    #                 self.dual['der'] = ders
-    #                 self.dual['val'] = vals
-    #                 # return ders
-    #         #for a list of numbers, evaluated at a single variable.
-    #         if (isinstance(input_val,list)):
-    #             for v in input_val:
-    #                 a = Dual(v)
-    #                 ders.append(self.fn(a).der)
-    #             self.dual['der'] = ders
-    #             self.dual['val'] = vals
-    #                 # return ders
-    #         else:
-    #             a = Dual(input_val)
-    #             dual_obj = self.fn(a)
-    #             self.dual['der'] = dual_obj.der
-    #             self.dual['val'] = dual_obj.val
+
 
     def get_der(self, val):
         """ Returns derivatives of the function evaluated at values given.
@@ -166,12 +93,24 @@ class AutoDiff:
         """
         vals = [] # a list to store function values
         if self.ndim >1:
-            for i in range(self.ndim):
-                def fxn(*args):
-                    return self.fn(*args)[i]
-                a = AutoDiff(fxn,ndim=1)
-                a.l=self.l
-                vals.append(a.get_val(val))
+            vals = []
+            if any(isinstance(el, list) for el in val):
+                for element in val:
+                    element_array = []
+                    for i in range(self.ndim):
+                        def fxn(*args):
+                            return self.fn(*args)[i]
+                        a = AutoDiff(fxn,ndim=1)
+                        a.l=self.l
+                        element_array.append(a.get_val(element))
+                    vals.append(element_array)
+            else:
+                for i in range(self.ndim):
+                    def fxn(*args):
+                        return self.fn(*args)[i]
+                    a = AutoDiff(fxn,ndim=1)
+                    a.l=self.l
+                    vals.append(a.get_val(val))
             return vals
         else:
             if self.l >= 2: # 2 or more parameters
@@ -183,25 +122,15 @@ class AutoDiff:
                     return list_val
                 if self.l != len(val):
                     raise Exception('Function requires {} values that correspond to the multiple variables'.format(self.l))
-                # else:
-                #     #for a list of numbers, evaluated at different variables.
-                #     for i in range(self.l):
-                #         new_val = val.copy()
-                #         new_val[i] = Dual(new_val[i])
-                #         v = self.fn(*new_val)
-                #         #Check if variable is in the function. (E.g., function paramaters are x, y and function is x.)
-                #         if type(v) is Dual:
-                #             vals.append(self.fn(*new_val).val)
-                #         else:
-                #             vals.append(0)
-                #     return vals
+                else:
+                    return self.fn(*val)
+
             #for a list of numbers, evaluated at a single variable.
             if (isinstance(val,list)):
                 for v in val:
                     a = Dual(v)
                     vals.append(self.fn(a).val)
                 return vals
-
             else: # just 1 parameter
                 a = Dual(val)
                 return self.fn(a).val
