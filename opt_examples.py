2867471acb49c436413f4de7a601f77ec7c9e083
Pat Sukhum <patsukhum@Pats-MacBook-Pro.local>
2018-12-09 21:05:06 -0500

    opt examples

--- opt_examples.py
+++ opt_examples.py
@@ -4,24 +4,27 @@ import numpy as np
 from sklearn.datasets import make_regression, make_classification
 
 
-# # Creates a regression problem with 3 features
-# X,y,coef = make_regression(n_features=3,n_informative=3,coef=True)
-#
-# # User defined loss function
-# def user_mse(y,y_preds):
-#     return (y-y_preds)**2
-#
-#
-# opt = Optimizer(loss=user_mse,optimizer='sgd',regularizer='ridge',lam=0.01)
-# opt.fit(X,y,iters=1000)
-#
-# print('Actual Values:')
-# print('======')
-# print(coef)
-#
-# print('Computed Values:')
-# print('======')
-# print(opt.coefs)
+# Creates a regression problem with 3 features
+X,y,coef = make_regression(n_features=3,n_informative=3,coef=True)
+
+# User defined loss function
+def user_mse(y,y_preds):
+    return (y-y_preds)**2
+
+
+opt = Optimizer(loss=user_mse,optimizer='sgd',regularizer='ridge',lam=0.01)
+opt.fit(X,y,iters=2000)
+
+print('Actual Values:')
+print('======')
+print(coef)
+
+
+print('Computed Values:')
+print('======')
+print(opt.coefs)
+print(opt.score(X,y))
+
 
 # Creates a classification problem with 3 features
 X2,y2 = make_classification(n_features=3,n_informative=3,n_redundant=0)
